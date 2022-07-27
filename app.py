f15b1047419941a3f0f4ff117f5275d3d014df64
Chris Limson <chrimson@gmail.com>
2022-07-27 10:07:48 -0400

    Normalize layout

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ classax_bp = Blueprint('classax', __name__, static_folder='', template_folder=''
 
 @classax_bp.route('/', methods=['GET'])
 def main():
-  return render_template('classax.html')
+  return render_template('common.html', content='classax.html')
 
 @classax_bp.route('/', methods=['POST'])
 def timer():
